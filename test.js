4226a2d280599b25c74c7e457f9b7843e200d1db
Harsh Vishwakarma <harshprogrammer782@gmail.com>
2022-11-18 09:34:10 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-// test file
+const Quote = require('inspirational-quotes');
+const cow = require("cowsay")
+
+
+// Quote.getRandomQuote(); // return any random quote
+
+
+console.log(cow.say({
+    text : Quote.getRandomQuote(),
+    e : "oO",
+    T : "U "
+}));
